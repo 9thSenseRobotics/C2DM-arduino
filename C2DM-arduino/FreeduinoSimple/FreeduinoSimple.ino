1aac8dd98aa0ec0cb70ad113e649e29835de1d2b
Dan <dan@9thsense.com>
2012-02-15 15:47:58 -0500

    created a simple sketch to demo how the C2DM-arduino stuff works

---
+++ C2DM-arduino/FreeduinoSimple/FreeduinoSimple.ino
@@ -0,0 +1,41 @@
+/* Freeduino Simple sketch
+* Serial speed is 57500 because Arduino serial monitor locks at 115200
+*/
+
+#include <Max3421e.h>
+#include <Usb.h>
+#include <FHB.h>
+
+
+AndroidAccessory acc("Google, Inc.",
+		     "DemoKit",
+		     "DemoKit Arduino Board",
+		     "1.0",
+		     "http://www.android.com",
+		     "0000000012345678");
+
+void setup()
+{
+	Serial.begin(57600);
+        
+        pinMode(3, OUTPUT);
+	digitalWrite(3, 1);
+
+	acc.powerOn();
+}
+
+void loop()
+{
+	byte msg[3];
+	if (acc.isConnected()) {
+	  int len = acc.read(msg, sizeof(msg), 1);
+          if (len > 0) {
+	    if (msg[0] == 0x2) {
+        	if (msg[1] == 0x0) analogWrite(3, msg[2]);
+                else analogWrite(3,20);
+            }
+          }
+        }
+	delay(10);
+}
+
